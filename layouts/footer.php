9205432b0627b7ec3479a1777595b1f1f9d43cd5
Enzo Styan Nazareno <100776806+EnzoStyan@users.noreply.github.com>
2025-01-18 10:08:07 +0700

    Add files via upload

---
+++ layouts/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+        <strong>Copyright ©
+            <script>
+                document.write(new Date().getFullYear())
+            </script>
+        <div class="float-right d-none d-sm-inline-block">
+            <b>Version</b> 1.0.0
+        </div>
+    </footer>
